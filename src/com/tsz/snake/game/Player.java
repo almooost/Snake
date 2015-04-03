1e06219f9dc4324877ac1b4483ccca93047e5756
almooost <sam.almost@gmail.com>
2015-04-03 22:34:33 +0200

    Added Player class to plain implemented classes

--- src/com/tsz/snake/game/Player.java
+++ src/com/tsz/snake/game/Player.java
@@ -1,5 +1,32 @@
 package com.tsz.snake.game;
 
-public class Player {
+import com.tsz.snake.store.HighScore;
+import com.tsz.snake.store.ScoreItem;
 
+/**
+ * Represents the snake player
+ * @author Sam
+ *
+ */
+public class Player 
+{
+	private int id;
+	private String name;
+	private int points;
+	private HighScore HighScore;
+
+	public Player(String name)
+	{
+		this.name = name;
+		this.id = 1;
+		this.points = 0;
+	}
+	
+	/**
+	 * Create KeyPress event
+	 */
+	public void KeyPress()
+	{
+		
+	}
 }
